3222de06d92e25ee14ba2026e43271265d4c43e6
Danesh <naikdanesh2@gmail.com>
2024-09-05 02:06:26 +0530

    initial commit

---
+++ src/main/java/com/danesh/pongo/PongoApplication.java
@@ -0,0 +1,13 @@
+package com.danesh.pongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongoApplication.class, args);
+	}
+
+}
